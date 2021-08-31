8b55f127b29f02ac6c7dfd1347473f434aa3952a
ivan.gorobenko <igorobenko@octo.com>
2021-08-31 10:59:08 +0200

    fix : test hook de pre-commit

--- tst/application/mapper/MessageDataMapper.spec.ts
+++ tst/application/mapper/MessageDataMapper.spec.ts
@@ -1,19 +1,18 @@
 import {expect} from "chai";
-import {Message} from "../../../src/domain/agregat/Message";
 import {MessageDataMapper} from "../../../src/application/mapper/MessageDataMapper";
 import {MessageDB} from "../../../src/application/repos/MessageDB";
-
+import {Message} from "../../../src/domain/agregat/Message";
 
 describe("MessageDataMapper", () => {
     it("doit permettre de mapper un objet MessageDB venant de la persistence vers une entité Message", function () {
         //GIVEN
-        const messageDB: MessageDB = new MessageDB("1", "Message 1", 123)
+        const messageDB: MessageDB = new MessageDB("1", "Message 1", 123);
         //WHEN
         const message: Message = MessageDataMapper.mapFromDBToDomain(messageDB);
         //THEN
-        expect(message.contenu).to.be.equal(messageDB.contenu)
-        expect(message.id).to.be.equal(messageDB.id)
-        expect(message.timestamp).to.be.equal(messageDB.timestamp)
+        expect(message.contenu).to.be.equal(messageDB.contenu);
+        expect(message.id).to.be.equal(messageDB.id);
+        expect(message.timestamp).to.be.equal(messageDB.timestamp);
     });
     it("doit permettre de mapper un objet Message venant de la persistence vers MessageDB", function () {
         //GIVEN
@@ -21,8 +20,8 @@ describe("MessageDataMapper", () => {
         //WHEN
         const messageDB: MessageDB = MessageDataMapper.mapFromDomainToDB(message);
         //THEN
-        expect(messageDB.contenu).to.be.equal(message.contenu)
-        expect(messageDB.id).to.be.equal(message.id)
-        expect(messageDB.timestamp).to.be.equal(message.timestamp)
+        expect(messageDB.contenu).to.be.equal(message.contenu);
+        expect(messageDB.id).to.be.equal(message.id);
+        expect(messageDB.timestamp).to.be.equal(message.timestamp);
     });
-})
+});
--- tst/domain/query/MessagesQueryHandler.spec.ts
+++ tst/domain/query/MessagesQueryHandler.spec.ts
@@ -1,20 +1,19 @@
-import {Message} from "../../../src/domain/agregat/Message";
 import {expect} from "chai";
-import {Result} from "../../../src/core/Result";
-import {MessagesQuery, MessagesQueryHandler} from "../../../src/domain/query/MessagesQueryHandler";
-import {MessageRepository} from "../../../src/application/repos/MessageRepository";
 import {InMemoryMessageRepositoryImpl} from "../../../src/application/repos/InMemoryMessageRepositoryImpl";
-
+import {MessageRepository} from "../../../src/application/repos/MessageRepository";
+import {Result} from "../../../src/core/Result";
+import {Message} from "../../../src/domain/agregat/Message";
+import {MessagesQuery, MessagesQueryHandler} from "../../../src/domain/query/MessagesQueryHandler";
 
 describe("Query de message", () => {
-    it('doit récupérer tous les messages existant', function () {
+    it("doit récupérer tous les messages existant", function () {
         //GIVEN
         const repository: MessageRepository = new InMemoryMessageRepositoryImpl();
         const messageOrError1: Result<Message> = Message.create("1", "Message 1", 123);
         const messageOrError2: Result<Message> = Message.create("2", "Message 2", 124);
 
-        repository.save("1", messageOrError1.getValue())
-        repository.save("2", messageOrError2.getValue())
+        repository.save("1", messageOrError1.getValue());
+        repository.save("2", messageOrError2.getValue());
         const messagesQueryHandler = new MessagesQueryHandler(repository);
         //WHEN
         const resultOrError = messagesQueryHandler.handle(new MessagesQuery());
@@ -41,7 +40,7 @@ describe("Query de message", () => {
         const resultOrError = messagesQueryHandler.handle(new MessagesQuery());
 
         //THEN
-        expect(resultOrError.isFailure).to.be.false;
+        expect(resultOrError.isFailure).to.be.true;
         const messages: Message[] = resultOrError.getValue();
         expect(messages.length).to.be.equals(0);
     });
