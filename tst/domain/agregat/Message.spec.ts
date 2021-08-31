9287b8372c5ef8809b82a1992f11ee8b20cd3bc9
ivan.gorobenko <igorobenko@octo.com>
2021-08-31 10:47:25 +0200

    fix : test hook de pre-commit

--- tst/domain/agregat/Message.spec.ts
+++ tst/domain/agregat/Message.spec.ts
@@ -1,10 +1,9 @@
-import {Message} from "../../../src/domain/agregat/Message";
 import {expect} from "chai";
 import {Result} from "../../../src/core/Result";
-
+import {Message} from "../../../src/domain/agregat/Message";
 
 describe("Agrégat Message", () => {
-    it('doit créer un message avec une date, contenu et identifiant', function () {
+    it("doit créer un message avec une date, contenu et identifiant", function () {
         //GIVEN
         const timestamp = 123;
         const contenu = "Mon message 1";
@@ -21,26 +20,26 @@ describe("Agrégat Message", () => {
         //GIVEN
         //WHEN
         // @ts-ignore
-        const messageOuErreur1: Result<Message> = Message.create(undefined,"Mon message 1",1);
+        const messageOuErreur1: Result<Message> = Message.create(undefined, "Mon message 1", 1);
 
         //THEN
         expect(messageOuErreur1.isFailure).to.be.true;
     });
-    it('ne doit pas permettre de créer un message avec le contenu vide', function () {
+    it("ne doit pas permettre de créer un message avec le contenu vide", function () {
         //GIVEN
         //WHEN
-        const messageOuErreurCasVide: Result<Message> = Message.create("123","",1);
-        const messageOuErreurCasIndéfini: Result<Message> = Message.create("123",undefined,1);
+        const messageOuErreurCasVide: Result<Message> = Message.create("123", "", 1);
+        const messageOuErreurCasIndéfini: Result<Message> = Message.create("123", undefined, 1);
 
         //THEN
         expect(messageOuErreurCasVide.isFailure).to.be.true;
         expect(messageOuErreurCasIndéfini.isFailure).to.be.true;
     });
-    it('ne doit permettre de créer un message avec une date vide', function () {
+    it("ne doit permettre de créer un message avec une date vide", function () {
         //GIVEN
         //WHEN
         // @ts-ignore
-        const messageOuErreur: Result<Message> = Message.create("1","Mon message");
+        const messageOuErreur: Result<Message> = Message.create("1", "Mon message");
 
         //THEN
         expect(messageOuErreur.isFailure).to.be.true;
--- tst/domain/command/LaisserMessageCommandHandler.spec.ts
+++ tst/domain/command/LaisserMessageCommandHandler.spec.ts
@@ -1,21 +1,21 @@
 // @ts-ignore
 import chai, {expect} from "chai";
-import {Message} from "../../../src/domain/agregat/Message";
 import {Result} from "../../../src/core/Result";
-import {
-    LaisserMessage,
-    LaisserMessageCommandHandler
-} from "../../../src/domain/command/LaisserMessageCommandHandler";
+import {Message} from "../../../src/domain/agregat/Message";
+import {LaisserMessage, LaisserMessageCommandHandler} from "../../../src/domain/command/LaisserMessageCommandHandler";
 import {MessageLaisseEvent} from "../../../src/domain/event/MessageLaisseEvent";
 
-chai.should()
+chai.should();
 
 describe("LaisserMessageCommandHandler", () => {
-    it('doit enregistrer un message', function () {
+    it("doit enregistrer un message", function () {
         //GIVEN
         let savedMessage = undefined;
         const command = new LaisserMessage("Mon message");
-        const sut = new LaisserMessageCommandHandler({save: (id, message) => savedMessage = message}, {now: () => 123});
+        const sut = new LaisserMessageCommandHandler(
+            {save: (id, message) => (savedMessage = message)},
+            {now: () => 123}
+        );
 
         //WHEN
         sut.handle(command);
@@ -24,19 +24,21 @@ describe("LaisserMessageCommandHandler", () => {
         savedMessage.contenu.should.be.equals("Mon message");
         savedMessage.timestamp.should.be.equals(123);
         expect(savedMessage).to.be.an.instanceof(Message);
-
     });
-    it('doit enregistrer un agrégat Message avec un identifiant unique', function () {
+    it("doit enregistrer un agrégat Message avec un identifiant unique", function () {
         //GIVEN
         const ids = [];
         const command1 = new LaisserMessage("Mon message 1");
         const command2 = new LaisserMessage("Mon message 2");
         // @ts-ignore
-        const sut = new LaisserMessageCommandHandler({
-            save: (id, message) => {
-                ids.push(id);
-            }
-        }, {now: () => 123});
+        const sut = new LaisserMessageCommandHandler(
+            {
+                save: (id, message) => {
+                    ids.push(id);
+                },
+            },
+            {now: () => 123}
+        );
 
         //WHEN
         sut.handle(command1);
@@ -45,11 +47,14 @@ describe("LaisserMessageCommandHandler", () => {
         //THEN
         expect(ids[0]).to.not.be.equal(ids[1]);
     });
-    it('doit renvoyer un erreur en cas d echec de traitement de la commande', function () {
+    it("doit renvoyer un erreur en cas d echec de traitement de la commande", function () {
         //GIVEN
         let savedMessage: Message;
         const command = new LaisserMessage("");
-        const sut = new LaisserMessageCommandHandler({save: (id, message) => savedMessage = message}, {now: () => 123});
+        const sut = new LaisserMessageCommandHandler(
+            {save: (id, message) => (savedMessage = message)},
+            {now: () => 123}
+        );
 
         //WHEN
         const resultOrError: Result<any> = sut.handle(command);
@@ -57,11 +62,14 @@ describe("LaisserMessageCommandHandler", () => {
         //THEN
         expect(resultOrError.isFailure).to.be.true;
     });
-    it('doit renvoyer un événement si traitement de la commande a réussi', function () {
+    it("doit renvoyer un événement si traitement de la commande a réussi", function () {
         //GIVEN
         let savedMessage: Message;
         const command = new LaisserMessage("Mon message");
-        const sut = new LaisserMessageCommandHandler({save: (id, message) => savedMessage = message}, {now: () => 123});
+        const sut = new LaisserMessageCommandHandler(
+            {save: (id, message) => (savedMessage = message)},
+            {now: () => 123}
+        );
 
         //WHEN
         const resultOrError: Result<MessageLaisseEvent> = sut.handle(command);
@@ -70,5 +78,4 @@ describe("LaisserMessageCommandHandler", () => {
         expect(resultOrError.getValue()).to.be.an.instanceof(MessageLaisseEvent);
         expect(resultOrError.getValue().id).to.not.be.undefined;
     });
-
-})
+});
