9ee105725f7b0dc15352e80e4c4ee3c11a2817ae
ivan.gorobenko <igorobenko@octo.com>
2021-09-03 09:20:24 +0200

    fix : test hook de pre-commit

--- tst/infrastructure/http/routes/routes.spec.ts
+++ tst/infrastructure/http/routes/routes.spec.ts
@@ -1,21 +1,26 @@
 // @ts-ignore
-import chai,{expect} from "chai";
-import {buildApp} from "../../../../src/buildApp";
+import chai, {expect} from "chai";
 // @ts-ignore
 import chaiHttp from "chai-http";
+import {buildApp} from "../../../../src/buildApp";
 
 chai.should();
 chai.use(chaiHttp);
 
 describe("La configuration de routes", () => {
-    it('doit rendre accessible la route PUT /message', function () {
-        chai.request(buildApp()).put("/message",).send({ message: 'mon message' }).end((err,res)=>{
-            expect(res.status).to.be.equal(200)
-        })
+    it("doit rendre accessible la route PUT /message", function () {
+        chai.request(buildApp())
+            .put("/message")
+            .send({message: "mon message"})
+            .end((err, res) => {
+                expect(res.status).to.be.equal(200);
+            });
     });
-    it('doit rendre accessible la route GET /messages', function () {
-        chai.request(buildApp()).get("/messages",).end((err,res)=>{
-            expect(res.status).to.be.equal(200)
-        })
+    it("doit rendre accessible la route GET /messages", function () {
+        chai.request(buildApp())
+            .get("/messages")
+            .end((err, res) => {
+                expect(res.status).to.be.equal(200);
+            });
     });
 });
