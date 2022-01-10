46f01529405676409e5ee5af1b868b5b44ea07e5
ivan.gorobenko <igorobenko@digikare.com>
2022-01-10 16:30:01 +0100

    refactor: enlever le FIX car déjà OK. simplifier structure tests

--- src/infrastructure/http/routes/communicationRoutes.spec.ts
+++ src/infrastructure/http/routes/communicationRoutes.spec.ts
@@ -18,54 +18,49 @@ describe("La configuration de routes de communication", () => {
         testContext.app = app;
         testContext.repositories = repositories;
     });
-    describe("dans le contexte de messages", () => {
-        it("doit rendre accessible la route PUT /message", done => {
-            chai.request(testContext.app)
-                .put("/message")
-                .send({message: "mon message"})
-                .end((err, res) => {
-                    expect(res.status).to.not.equal(404);
-                    expect(res.status).to.be.equal(204);
-                    done();
-                });
-        });
-        it("doit rendre accessible la route GET /messages", done => {
-            chai.request(testContext.app)
-                .get("/messages")
-                .end((err, res) => {
-                    expect(res.status).to.be.equal(200);
-                    done();
-                });
-        });
-        it("doit rendre accessible la route DELETE /messages:id", done => {
-            chai.request(testContext.app)
-                .delete("/messages/1")
-                .end((err, res) => {
-                    expect(res.status).to.be.equal(204);
-                    done();
-                });
-        });
+    it("doit rendre accessible la route PUT /message", done => {
+        chai.request(testContext.app)
+            .put("/message")
+            .send({message: "mon message"})
+            .end((err, res) => {
+                expect(res.status).to.not.equal(404);
+                expect(res.status).to.be.equal(204);
+                done();
+            });
     });
-    //FIXME: il y a un souci avec le contexte de tests en enlevant le describe de messages le test plante
-    describe(`dans le contexte d'alertes`, () => {
-        it("doit rendre accessible la route PUT /alerte", done => {
-            chai.request(testContext.app)
-                .put("/alerte")
-                .send({alerte: {lieu: {latitude: 1.2, longitude: 2.3}, timestamp: 123}})
-                .end((err, res) => {
-                    expect(res.status).to.be.equal(204);
-                    done();
-                });
-        });
-        it("doit rendre accessible la route GET /alerte-active", done => {
-            const alerteRepository = testContext.repositories.alerteRepository;
-            alerteRepository.save(Alerte.lancer("123", new Lieu(1, 2), 123));
-            chai.request(testContext.app)
-                .get("/alerte-active")
-                .end((err, res) => {
-                    expect(res.status).to.be.equal(200);
-                    done();
-                });
-        });
+    it("doit rendre accessible la route GET /messages", done => {
+        chai.request(testContext.app)
+            .get("/messages")
+            .end((err, res) => {
+                expect(res.status).to.be.equal(200);
+                done();
+            });
+    });
+    it("doit rendre accessible la route DELETE /messages:id", done => {
+        chai.request(testContext.app)
+            .delete("/messages/1")
+            .end((err, res) => {
+                expect(res.status).to.be.equal(204);
+                done();
+            });
+    });
+    it("doit rendre accessible la route PUT /alerte", done => {
+        chai.request(testContext.app)
+            .put("/alerte")
+            .send({alerte: {lieu: {latitude: 1.2, longitude: 2.3}, timestamp: 123}})
+            .end((err, res) => {
+                expect(res.status).to.be.equal(204);
+                done();
+            });
+    });
+    it("doit rendre accessible la route GET /alerte-active", done => {
+        const alerteRepository = testContext.repositories.alerteRepository;
+        alerteRepository.save(Alerte.lancer("123", new Lieu(1, 2), 123));
+        chai.request(testContext.app)
+            .get("/alerte-active")
+            .end((err, res) => {
+                expect(res.status).to.be.equal(200);
+                done();
+            });
     });
 });
