1548fd8f6078e0d5e1501c6e77db71fe7a9ebec8
ivangorobenko <49490325+ivangorobenko@users.noreply.github.com>
2022-01-24 22:19:24 +0100

    chore : rolling back

--- src/buildApp.ts
+++ src/buildApp.ts
@@ -32,20 +32,7 @@ export const buildApp = (repositories: Repositories): Application => {
     app.use(express.json());
     app.use(express.urlencoded({extended: false}));
     app.use(cors({origin: ["http://localhost:3000", "https://alzaide.herokuapp.com/"]}));
-    // Add headers before the routes are defined
-    app.use(function (req, res, next) {
-        // Website you wish to allow to connect
-        res.setHeader("Access-Control-Allow-Origin", "https://alzaide.herokuapp.com/");
 
-        // Request methods you wish to allow
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-
-        // Request headers you wish to allow
-        res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-
-        // Pass to next layer of middleware
-        next();
-    });
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, "public")));
 
